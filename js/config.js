7431b63e9ac498dec834d5abd8aa95b7d678dfb0
Jdaniel095 <daniel.leandro.10.12@gmail.com>
2025-11-01 23:51:03 -0500

    Modify APP_CONFIG with new SHEET_URL
    Updated SHEET_URL to a new endpoint.

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window.APP_CONFIG = {
   DRIVE_FOLDER_ID: "1SykigPCl4e003n6i2_PbopBrq22droo4",
   API_KEY: "AIzaSyAzq7mmTZTExpbetLRCkbopY5pDPBvOki4",
   SCOPES: "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.readonly",
-  SHEET_URL: "https://script.google.com/macros/s/AKfycbxGpE5kTYXhcfZqknAggpuGsxPR513oABHkaqwsEM9CCi3edVCZpVWobrYSA-scGVj7Ww/exec"
+  SHEET_URL: "https://script.google.com/macros/s/AKfycbzH9O_Xq5HOU7j1eVLZOERcA51kOPLt0RztK7sINY1uHiZH8oW2T9thyxEBssRsG-F7WQ/exec"
 };
+
 
 
 
